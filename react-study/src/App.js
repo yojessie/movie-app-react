44566b6c6a1733eadc3e1c2d4cee8e598b5f7dad
yojessie <sprout.jessie@gmail.com>
2022-06-09 22:41:06 +0900

    🚧  Create styled components

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,8 +1,45 @@
+// npm i styled-components
+// install the extension : vscode-styled-components
+import styled from "styled-components";
+
+const BigDiv = styled.div`
+  display: flex;
+`;
+
+const DivOne = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: tomato;
+  width: 300px;
+  height: 300px;
+`;
+
+const DivTwo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: bisque;
+  width: 300px;
+  height: 300px;
+`;
+
+const Text = styled.h1`
+  color: white;
+`;
+
+// styled component makes a random class name
+
 function App() {
   return (
-    <div>
-      <h1>HELLO</h1>
-    </div>
+    <BigDiv>
+      <DivOne>
+        <Text>HELLO</Text>
+      </DivOne>
+      <DivTwo>
+        <Text>WORLD</Text>
+      </DivTwo>
+    </BigDiv>
   );
 }
 
