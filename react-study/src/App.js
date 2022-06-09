7b55f510751595f6edf2245c0eff82ec72da92aa
yojessie <sprout.jessie@gmail.com>
2022-06-09 23:58:31 +0900

    🚧  Use 'As' and Attrs on styled components

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,9 +1,11 @@
 // npm i styled-components
 // install the extension : vscode-styled-components
 import styled from "styled-components";
+import "./App.css";
 
 const BigDiv = styled.div`
   display: flex;
+  margin-bottom: 40px;
 `;
 
 const Box = styled.div`
@@ -23,19 +25,59 @@ const Text = styled.h1`
   color: white;
 `;
 
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 32px;
+  color: white;
+  background-color: tomato;
+  border-radius: 4px;
+  margin-right: 8px;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+const Input = styled.input.attrs({
+  required: true,
+  placeholder: "test styled component attrs",
+})`
+  background-color: #f2f2f2;
+  width: 300px;
+  height: 32px;
+  margin-right: 8px;
+  padding: 8px;
+  border-radius: 4px;
+`;
+// styled components can use 'attrs' to give a property for all components
 // styled component makes a random class name
 
 function App() {
   return (
-    <BigDiv>
-      <Box bgColor="tomato">
-        <Text>HELLO</Text>
-      </Box>
-      <Circle bgColor="bisque">
-        <Text>WORLD</Text>
-      </Circle>
-    </BigDiv>
+    <div>
+      <BigDiv>
+        <Box bgColor="tomato">
+          <Text>HELLO</Text>
+        </Box>
+        <Circle bgColor="bisque">
+          <Text>WORLD</Text>
+        </Circle>
+      </BigDiv>
+      <BigDiv as="section">
+        <Button>Button</Button>
+        <Button as="a" href="/">
+          Anchor
+        </Button>
+      </BigDiv>
+      <BigDiv>
+        <Input />
+        <Input />
+      </BigDiv>
+    </div>
   );
 }
+
+// component에서 as 속성을 사용하면 같은 스타일 컴포넌트를 다른 태그로 활요할 수 있다.
 
 export default App;
