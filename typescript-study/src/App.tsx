1191c06a55e85f2c0f73e7f2e6aeff598eded97c
yojessie <sprout.jessie@gmail.com>
2022-06-12 00:25:40 +0900

    🚧  Reset the files

--- typescript-study/src/App.tsx
+++ typescript-study/src/App.tsx
@@ -1,23 +1,10 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>HELLO</Title>
-    </Wrapper>
+    <div>
+      <Circle />
+    </div>
   );
 }
 
--- typescript-study/src/index.tsx
+++ typescript-study/src/index.tsx
@@ -1,25 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#2f2f2f",
-};
-
-const lightTheme = {
-  textColor: "#2f2f2f",
-  backgroundColor: "whitesmoke",
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
